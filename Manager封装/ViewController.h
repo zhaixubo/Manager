bdf3f71c354a0b4ef999900447e8649f71291ce3
zhaixubo <464574487@qq.com>
2022-10-08 18:56:14 +0800

    Initial Commit

---
+++ Manager封装/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Manager封装
+//
+//  Created by 翟旭博 on 2022/10/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Manager封装/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Manager封装
+//
+//  Created by 翟旭博 on 2022/10/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
